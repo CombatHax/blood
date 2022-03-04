89fd688cdeeb73e06fbb5770b9377ef19b0362ba
CombatHax <92267016+CombatHax@users.noreply.github.com>
2022-03-04 16:31:36 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ let pos = []
 const sprites = [];
 let bldAmt = 0;
 let loaded = 0;
+let move = [];
+let temp = false;
 
 for(let i = 0; i < 4; i++) {
     let img = new Image(16, 16);
@@ -31,7 +33,14 @@ function intro() {
     ctx.strokeText("The goal of this game is to test as much blood as you can!", 5, 50, 995);
     ctx.fillStyle = "#FF0000";
     ctx.fillText("The goal of this game is to test as much blood as you can!", 5, 50, 995);
-    ctx.strokeText("You can move with W, A, S, and D", 300, 350, 400);
+    ctx.fillStyle = "#0000FF";
+    ctx.fillText("You can move with W, A, S, and D", 250, 342, 500);
+    ctx.fillStyle = `rgba(255, 0, 0, ${Math.floor(Date.now()) / 1000 % 2})`;
+    ctx.fillText("Press any button to start the game", 250, 550, 500);
+    if(temp) {
+        draw();
+        return;
+    }
     window.requestAnimationFrame(intro);
 }
 
@@ -42,6 +51,44 @@ function draw() {
     ctx.fillStyle = "#FFFFFF";
     ctx.fill();
     ctx.stroke();
-    ctx.drawImage(sprites[bldAmt]);
+    pos[0] += move[0] - move[0] * 2;
+    pos[1] += move[1] - move[1] * 2;
+    ctx.drawImage(sprites[bldAmt], pos[0], pos[1]);
     window.requestAnimationFrame(draw);
+}
+
+window.onkeydown = function(e) {
+    switch(e.key) {
+        case "w":
+            moves[1]--;
+            break;
+        case "s":
+            move[1]++;
+            break;
+        case "a":
+            move[0]--;
+            break;
+        case "d":
+            move[0]++;
+            break;
+    }
+}
+window.onkeyup = function(e) {
+    switch(e.key) {
+        case "w":
+            moves[1] = 0;
+            break;
+        case "s":
+            move[1] = 0;
+            break;
+        case "a":
+            move[0] = 0;
+            break;
+        case "d":
+            move[0] = 0;
+            break;
+    }
+    if(!temp) {
+        temp = true;
+    }
 }
